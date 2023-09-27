c7066675440597d79d27a6fd7f71b40f5ee5ebb9
anzhaurova <esteranzhi@gmail.com>
2023-09-27 09:16:36 +0300

    feat:create model and added seeds

--- app/Models/Booklist.php
+++ app/Models/Booklist.php
@@ -2,52 +2,10 @@
 
 namespace App\Models;
 
-class BookList {
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'title' => 'booklist One',
-                'description' => 'Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur'
-            ],
-            [
-                'id' => 2,
-                'title' => 'booklist Two',
-                'description' => 'Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur'
-            ],
-            [
-                'id' => 3,
-                'title' => 'booklist Three',
-                'description' => 'Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur'
-            ],
-        ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id) {
-        $booklists = self::all();
-
-        foreach ($booklists as $booklist) {
-            if($booklist['id'] == $id) {
-                return $booklist;
-            }
-        }
-    } 
+class Booklist extends Model
+{
+    use HasFactory;
 }
