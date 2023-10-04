ec9d95e05ce0f09d37b584a6969465ef150b5e6c
anzhaurova <esteranzhi@gmail.com>
2023-10-04 22:14:54 +0300

    feat:begin to work with styles

--- resources/views/booklists.blade.php
+++ resources/views/booklists.blade.php
@@ -1,20 +1,23 @@
 @extends('layout')
 
 @section('content')
-<h1>{{$heading}}</h1>
+<div class="mba:grid mba:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"></div>
 
 @unless(count($booklists) == 0)
 @foreach ($booklists as $booklist)
+<div class="flex">
     <h2>
-        <a href="/booklists/{{$booklist['id']}}">{{$booklist['title']}}</a>
+        <a href="/booklists/{{$booklist['id']}}">{{$booklist->title}}</a>
     </h2>
-    <p>
-        {{$booklist['description']}}
-    </p>
+    <div class="text-lg mt-4">
+        {{$booklist->description}}
+    </div>
+</div>
+    
 @endforeach
 
 @else 
 <p>No booklists found</p>
 @endunless
-
+</div>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,12 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Booklists</title>
+    <link rel="icon" href="images/favicon.ico" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+    integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        laravel: '#ef3b2d',
+                    },
+                },
+            },
+        }
+    </script>
+    <title>Booklists | Find or Create your own booklists</title>
 </head>
-<body>
+<body class="mb-48">
+    <nav class="flex justify-between items-center mb-4">
+
+    </nav>
     <h1>My Booklist App</h1>
     {{-- VIEW OUTPUT--}}
-    @yield('content')
+
+    <main>
+        @yield('content')
+    </main>
+    <footer>
+        <p>Copyright &copy; 2023, All Rights reserved</p>
+
+        <a href="create.html" class="absolute top-1/3 right-10 bg-olive py-2 px-5">Create booklists</a>
+    </footer>
 </body>
 
-</html>
+</html> 
