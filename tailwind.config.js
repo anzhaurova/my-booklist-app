c210d8698b5518850d8378a7dd0f854038ddc094
anzhaurova <esteranzhi@gmail.com>
2023-10-05 11:27:52 +0300

    feat:add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        laravel: '#ef3b2d',
+      },
+    },
+  },
+  plugins: [],
+}
