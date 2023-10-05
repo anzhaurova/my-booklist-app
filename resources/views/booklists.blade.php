84f8b355c812e59450e4e23dee0e5ea075d2abc8
anzhaurova <esteranzhi@gmail.com>
2023-10-05 20:23:18 +0300

    feat:styles

--- resources/views/booklists.blade.php
+++ resources/views/booklists.blade.php
@@ -1,18 +1,31 @@
 @extends('layout')
 
 @section('content')
-<div class="mba:grid mba:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"></div>
+@include('partials._list')
+
+<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
 @unless(count($booklists) == 0)
+
 @foreach ($booklists as $booklist)
-<div class="flex">
+<div class="bg-gray-50 border border-gray-400 rounded p-6">
+    <div class="flex">
+        <div>
+            <h3 class="text-2xl">
+                <a href="show.html">{{$booklist->title}}</a>
+            </h3>
+            <div class="text-xl  mb-4">{{$booklist->description}}</div>
+        </div>
+    </div>
+</div>
+{{-- <div class="flex">
     <h2>
         <a href="/booklists/{{$booklist['id']}}">{{$booklist->title}}</a>
     </h2>
     <div class="text-lg mt-4">
         {{$booklist->description}}
     </div>
-</div>
+</div> --}}
     
 @endforeach
 
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,21 +4,55 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-{{-- <link rel="stylesheet" href="{{ asset('css/app/css') }}"> --}}
+    <link rel="icon" href="images/favicon.ico" />
+
     @vite('resources/css/app.css')
-    <title>Booklists | Find or Create your own booklists</title>
+
 </head>
 <body class="mb-48">
-    <h1>My Booklist App</h1>
+
     {{-- VIEW OUTPUT--}}
-    
+
+    <nav class="flex justify-between items-center mb-0">
+        <a href="index.html">
+            <img class="w-24" src="{{asset('images/compact-logo.png')}}" alt="" class="logo"/>
+        </a>
+
+        <div class="z-10">
+            <h1 class="text-4xl font-bold uppercase text-black ">
+              My Book<span class="text-black">Lists</span>
+            </h1>
+            <p class="text-2xl text-gray-200 my-0">Find or Create your own booklists</p>
+            <div>
+              @auth
+              @else
+              {{-- <a href="/register"
+                class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black">Sign
+                Up to List a Gig</a> --}}
+              @endauth
+            </div>
+          </div>
+
+        <ul class="flex space-x-6 mr-6 text-lg">
+            <li>
+                <a href="register.html" class="hover:text-laravel">
+                    <i class="fa-solid fa-user-plus"></i> Register
+                </a>
+            </li>
+            <li>
+                <a href="login.html" class="hover:text-laravel">
+                    <i class="fa-solid fa-arrow-right-to-bracket"></i>Login
+                </a>
+            </li>
+        </ul>
+    </nav>
+
     <main>
         @yield('content')
     </main>
-    <footer>
-        <p>Copyright &copy; 2023, All Rights reserved</p>
-        <a href="create.html" class="absolute top-1/3 right-10 bg-olive py-2 px-5">Create booklists</a>
+    <footer class="fixed bottom-0 left-0 w-full flex items-center justify-start bg-black text-white h-24 mt-24 opacity-90 md:justify-center">
+        <p class="ml-2">Copyright &copy; 2023 all rights reserved</p>
+        <a href="create.html" class="absolute top-1/3 right-10 text-white py-2 px-5">Create Booklist</a>
     </footer>
 </body>
-
-</html> 
+</html>
