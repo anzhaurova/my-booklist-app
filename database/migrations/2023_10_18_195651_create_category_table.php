2d20631a0e27df6ce6347ea3f59e88b860664b54
anzhaurova <esteranzhi@gmail.com>
2023-10-20 18:31:54 +0300

    feat:add controllers for tables

--- database/migrations/2023_10_18_195651_create_category_table.php
+++ database/migrations/2023_10_18_195651_create_category_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('category', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('parent_id');
             $table->timestamps();
         });
     }
